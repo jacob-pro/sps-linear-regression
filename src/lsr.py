9e05af833cd2a02c99baa4699070654cf34ca689
Jacob Halsey <jacob@jhalsey.com>
2020-03-16 18:38:29 +0000

    more

--- src/lsr.py
+++ src/lsr.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 from numpy import ndarray
 
 
-def load_points_from_file(filename):
+def load_points_from_file(filename: str) -> Tuple[ndarray, ndarray]:
     """Loads 2d points from a csv called filename
     Args:
         filename : Path to .csv file
@@ -48,34 +48,73 @@ def group_points_into_segments(xs: ndarray, ys: ndarray) -> List[Segment]:
     return list(map(lambda line: (xs_split[line], ys_split[line]), range(lines)))
 
 
-def lsr_fn(xs: ndarray, ys: ndarray, fn: Callable):
+class LsrResult(NamedTuple):
+    name: str
+    coefficients: ndarray
+    error: float
+    function: str
+
+
+# Float to string with prefixed sign
+def fts(x: float) -> str:
+    y = '{:+.2f}'.format(x)
+    return "{} {}".format(y[0:1], y[1:])
+
+
+# String representation of poly multiplier
+def xv(x: int) -> str:
+    if x == 0:
+        return ''
+    elif x == 1:
+        return " * x"
+    else:
+        return " * x^{}".format(x)
+
+
+def lsr_fn(xs: ndarray, ys: ndarray, fn: Callable) -> LsrResult:
     x_e = np.column_stack((np.ones(len(xs)), fn(xs)))
     v = np.linalg.inv(x_e.T.dot(x_e)).dot(x_e.T).dot(ys)
-    return v
+    a, b = v
+    y_hat = a + b * fn(xs)
+    e = float(np.sum((y_hat - ys) ** 2))
+    formatted = "{} {} * {}(x)".format(fts(a), fts(b), fn.__name__)[2:]
+    return LsrResult(fn.__name__, v, e, formatted)
 
 
-def lsr_polynomial(xs: ndarray, ys: ndarray, degree: int):
+def lsr_polynomial(xs: ndarray, ys: ndarray, degree: int) -> LsrResult:
     # x**0 = 1, creating the column of ones
     columns = list(map(lambda i: list(map(lambda x: x**i, xs)), range(degree + 1)))
     x_e = np.column_stack(columns)
     v = np.linalg.inv(x_e.T.dot(x_e)).dot(x_e.T).dot(ys)
+    y_hat = np.polyval(np.flip(v), xs)
+    e = float(np.sum((y_hat - ys) ** 2))
+    formatted = " ".join(map(lambda x: "{}{}".format(fts(v[x]), xv(x)), range(degree + 1)))[2:]
+    return LsrResult("polynomial", v, e, formatted)
 
-    a, b = v
-    y_hat = a + b * xs
-    e = np.sum((y_hat - ys) ** 2)
-    return v
 
+def compute(segments: List[Segment]) -> Tuple[List[LsrResult], float]:
 
-def compute(file_path: str, plot: bool) -> None:
-    (xs, ys) = load_points_from_file(file_path)
-    segments = group_points_into_segments(xs, ys)
+    max_poly = 4
+    bests: Dict[int, Tuple[List[LsrResult], float]] = {}
 
-    (xs1, ys1) = segments[0]
-    lsr_polynomial(xs1, ys1, 1)
-    lsr_fn(xs1, ys1, np.tan)
+    for degree in range(2, max_poly + 1):
+        temp = []
+        for (xs, ys) in segments:
+            results: List[LsrResult] = [
+                lsr_polynomial(xs, ys, 1),
+                lsr_polynomial(xs, ys, degree),
+                lsr_fn(xs, ys, np.sin),
+                lsr_fn(xs, ys, np.cos),
+                lsr_fn(xs, ys, np.tan),
+                lsr_fn(xs, ys, np.exp),
+                lsr_fn(xs, ys, np.reciprocal),
+                # lsr_fn(xs, ys, np.sqrt),
+            ]
+            temp.append(min(results, key=lambda r: r.error))
+        bests[degree] = (temp, sum(k.error for k in temp))
 
-    if plot:
-        view_data_segments(xs, ys)
+    best = min(bests.values(), key=lambda b: b[1])
+    return best
 
 
 def main(argv: List) -> None:
@@ -93,7 +132,13 @@ def main(argv: List) -> None:
         else:
             print("Unrecognised argument")
             exit(1)
-    compute(file_path, plot)
+
+    (xs, ys) = load_points_from_file(file_path)
+    segments = group_points_into_segments(xs, ys)
+    lines, error = compute(segments)
+    print(error)
+    if plot:
+        view_data_segments(xs, ys)
 
 
 if __name__ == "__main__":
